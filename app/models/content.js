b8509460f648fdccb3edec38fc2ecd1ca1a08827
Черемхин Александр <che603000@gmail.com>
2016-12-26 03:49:16 +0300

    validations #1

--- app/models/content.js
+++ app/models/content.js
@@ -9,29 +9,36 @@ export default DS.Model.extend(ValidatorMixin, {
     }
   }),
   body: DS.attr('string'),
-  // validate(){
-  //   //debugger;
-  //   const r = this._super(...arguments)
-  //   return r ? Ember.RSVP.resolve(r) : Ember.RSVP.reject(r);
-  // },
-  // save: function save() {
-  //   var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-  //
-  //   var _ref$validate = _ref.validate;
-  //   var validate = _ref$validate === undefined ? true : _ref$validate;
-  //
-  //   // if (!validate) {
-  //   //   return this._super();
-  //   // }
-  //
-  //   return this.validate()
-  //     .then(() => this._super())
-  //     .catch(() => createValidationError(this))
-  //
-  //   // if (this.validate()) {
-  //   //   return Ember.RSVP.resolve();
-  //   // }
-  //   //
-  //   // return Ember.RSVP.reject(createValidationError(this));
-  // }
+  validate(){
+    //debugger;
+    const r = this._super(...arguments)
+    let errors = this.get('errors');
+    return r ? Ember.RSVP.resolve(errors) : Ember.RSVP.reject(errors);
+  },
+  save: function save() {
+    var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+    var _ref$validate = _ref.validate;
+    var validate = _ref$validate === undefined ? true : _ref$validate;
+
+    // if (!validate) {
+    //   return this._super();
+    // }
+
+    return this.validate()
+      .then(() => {
+         return this._super()
+        }
+      )
+      .catch(() => {
+        return createValidationError(this)
+      })
+
+
+    // if (this.validate()) {
+    //   return Ember.RSVP.resolve();
+    // }
+    //
+    // return Ember.RSVP.reject(createValidationError(this));
+  }
 });
